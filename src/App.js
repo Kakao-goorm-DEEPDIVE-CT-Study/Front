57c9fcae51aa71883bbb67c5a0d79a6dee4b5cea
Teddy <ebzm00@naver.com>
2024-12-11 23:25:43 +0900

    소셜로그인 이미지 화면 추가 (241211)
    -- 소셜 로그인 구현

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import OptidayApp from './components/OptidayApp';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <OptidayApp/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+
+export default App;
+
